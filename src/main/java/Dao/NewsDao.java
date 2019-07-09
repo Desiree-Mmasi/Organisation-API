2a1226aac5b2561d71c0e1b058dc12f2707b6267
Moringa School <mmasidesiree9@gmail.com>
2019-07-09 14:47:34 +0300

    This commit will add the News interface

--- src/main/java/Dao/NewsDao.java
+++ src/main/java/Dao/NewsDao.java
@@ -1,4 +1,15 @@
 package Dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    void add(News news);
+
+    List<News> getAll();
+    List<News> getAllNewsByDepartment(int DepartmentId);
+
+    void deleteById(int id);
+    void clearAll();
 }
